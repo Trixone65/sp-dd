a273489e5bc98f2fb9d87282dd04b8b5c7f6417f
Trixone65 <145619392+Trixone65@users.noreply.github.com>
2025-04-11 00:34:49 +0100

    Delete RepairShop directory

--- RepairShop/src/main/java/com/repairsystem/dao/TicketDAO.java
+++
@@ -1,9 +0,0 @@
-package com.repairsystem.dao;
-
-import com.repairsystem.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketDAO extends JpaRepository<Ticket, Long> {
-}
--- RepairShop/src/main/java/com/repairsystem/dao/TicketRepository.java
+++
@@ -1,6 +0,0 @@
-package com.repairsystem.dao;
-import com.repairsystem.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
